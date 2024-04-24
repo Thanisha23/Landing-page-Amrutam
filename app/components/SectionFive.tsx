b582bffaea8a84caced172c4d0f0039aca572a07
Thanisha Belchada <thanishab77@gmail.com>
2024-04-24 23:15:53 +0530

    fix: minor changes

--- app/components/SectionFive.tsx
+++ app/components/SectionFive.tsx
@@ -7,7 +7,7 @@ import Card4 from './Card4'
 const SectionFive:React.FC = () => {
   return (
     <div className='h-[2320px] pt-[62px] w-[100%] bg-[#FFFAEC]'>
-        <div className=' text-[#3A643B] text-[32px] font-bold flex justify-center items-center w-[80%] mx-auto text-center pt-[61px]  '>What sets Ayurvedic consultations apart?</div>
+        <div className=' text-[#3A643B] text-[32px] font-bold flex justify-center items-center w-[80%] mx-auto text-center pt-[61px] pb-[2.5rem] '>What sets Ayurvedic consultations apart?</div>
         <Card1 />
         <div className='mt-[24px] rounded-[20px] flex justify-center items-center'>
             <Image src="/card1Image.png" width={328} height={285} alt='card Image'/>
